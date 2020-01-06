be9fd0b54863c05ef8ecb916560040e4dcd0692a
Cyrille DUPUYDAUBY <dupdob@users.noreply.github.com>
2020-01-06 23:13:20 +0100

    Update Program.cs

--- 2019/Program.cs
+++ 2019/Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventCalendar2019.Day20;
+﻿using AdventCalendar2019.Day22;
 
 namespace AdventCalendar2019
 {
@@ -6,7 +6,7 @@ namespace AdventCalendar2019
     {
         static void Main(string[] args)
         {
-            DupdobDay20.GiveAnswers();
+            DupdobDay22.GiveAnswers();
         }
     }
 }
