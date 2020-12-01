483f8043e6ae87b8150127b2c9a59219c68f4f18
Cyrille Dupuydauby <dupdob@gmail.com>
2020-12-01 08:52:33 +0100

    2020 Day 1 done

---
+++ AdventCalendar2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventCalendar2020.Day_1;
+
+namespace AdventCalendar2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new DupdobDay1();
+            test.Parse();
+            Console.WriteLine($"Day 1: {test.GiveAnswers()} & {test.GiveAnswer2()}");
+        }
+    }
+}
