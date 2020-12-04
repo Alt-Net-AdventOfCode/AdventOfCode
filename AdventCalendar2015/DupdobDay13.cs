ab1e2d4be10bd5e54c3c52c4ed640f9a6eb58876
Cyrille Dupuydauby <dupdob@gmail.com>
2020-12-04 09:26:07 +0100

    done 2015-12

---
+++ AdventCalendar2015/DupdobDay13.cs
@@ -0,0 +1,20 @@
+namespace AdventCalendar2015
+{
+    public class DupdobDay13
+    {
+        public void Parse()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object? Compute1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object? Compute2()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- AdventCalendar2015/Program.cs
+++ AdventCalendar2015/Program.cs
@@ -6,7 +6,9 @@ namespace AdventCalendar2015
     {
         private static void Main(string[] args)
         {
-            //GiveAllAnswers();
+            var day13 = new DupdobDay13();
+            day13.Parse();
+            Console.WriteLine("Day 13: (1) = {0} , (2) = {1}", day13.Compute1(), day13.Compute2());
         }
 
         private static void GiveAllAnswers()
@@ -18,7 +20,9 @@ namespace AdventCalendar2015
             Console.WriteLine("Day 10: (1) = {0} , (2) = {1}", day10.RepeatedLookAndSay(40), day10.RepeatedLookAndSay(10));
             var day11 = new DupdobDay11();
             day11.Parse();
-            Console.WriteLine("Day 10: (1) = {0} , (2) = {1}", day11.Compute1(), day11.Compute2());
-        }
+            Console.WriteLine("Day 11: (1) = {0} , (2) = {1}", day11.Compute1(), day11.Compute2());
+            var day12 = new DupdobDay12();
+            day12.Parse();
+            Console.WriteLine("Day 12: (1) = {0} , (2) = {1}", day12.Compute1(), day12.Compute2());        }
     }
 }
