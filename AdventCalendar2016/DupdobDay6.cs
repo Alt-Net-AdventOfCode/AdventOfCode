3bb5e64b20736585b32f69f3ac3526d08afa7420
Cyrille Dupuydauby <dupdob@gmail.com>
2021-12-02 06:29:29 +0100

    Day 6 _ 2016

---
+++ AdventCalendar2016/DupdobDay6.cs
@@ -0,0 +1,655 @@
+using System.Collections.Generic;
+using AOCHelpers;
+
+namespace AdventCalendar2016
+{
+    public class DupdobDay6 : DupdobDayWithTest
+    {
+        private List<string> _lines = new();
+        protected override void ParseLine(int index, string line)
+        {
+            _lines.Add(line);
+        }
+
+        public override object GiveAnswer1()
+        {
+            var result = string.Empty;
+            for (var i = 0; i < _lines[0].Length; i++)
+            {
+                var hit = new Dictionary<char, int>();
+                foreach (var line in _lines)
+                {
+                    if (!hit.ContainsKey(line[i]))
+                    {
+                        hit[line[i]] = 1;
+                    }
+                    else
+                    {
+                        hit[line[i]]++;
+                    }
+                }
+
+                var status = (0, ' ');
+                foreach (var entry in hit)
+                {
+                    if (entry.Value > status.Item1)
+                    {
+                        status.Item1 = entry.Value;
+                        status.Item2 = entry.Key;
+                    }
+                }
+
+                result += status.Item2;
+            }
+
+            return result;
+        }
+        public override object GiveAnswer2()
+        {
+            var result = string.Empty;
+            for (var i = 0; i < _lines[0].Length; i++)
+            {
+                var hit = new Dictionary<char, int>();
+                foreach (var line in _lines)
+                {
+                    if (!hit.ContainsKey(line[i]))
+                    {
+                        hit[line[i]] = 1;
+                    }
+                    else
+                    {
+                        hit[line[i]]++;
+                    }
+                }
+
+                var status = (int.MaxValue, ' ');
+                foreach (var entry in hit)
+                {
+                    if (entry.Value < status.Item1)
+                    {
+                        status.Item1 = entry.Value;
+                        status.Item2 = entry.Key;
+                    }
+                }
+
+                result += status.Item2;
+            }
+
+            return result;
+        }
+
+        protected override string Input => @"wxupppln
+ducindyd
+rhszlyfu
+kouqvpue
+nwntizie
+feyhuurg
+qlgzsmya
+svwijujv
+tcmsiuly
+vyrnynvz
+nmidjjxd
+ciduttzr
+nofnijfo
+ghskpifp
+yoyvsxct
+aqwzgouh
+ihhkdayn
+lmwyapsl
+nxkdwnoq
+hdkcvjrr
+ecxvdbeg
+sfybsvuu
+gygvlghh
+ivokuftm
+grolarfj
+tgzpjlca
+lojlcrks
+bmxogggc
+iablmcem
+cractnta
+mpdrqxea
+bdvrqbky
+zymlyyyk
+dhengvth
+vrvvfnoo
+ibemygpu
+yjetqqrc
+yhvpkxxc
+rqfzoakw
+uihcznsn
+blovcrpx
+dkrfqpco
+xytyahwu
+corlghco
+gisliobo
+qifxxizh
+uykqywcf
+pfnpbllg
+kqcmwcsf
+jgfujsse
+klmgeozq
+gpzgmjfr
+vievatyq
+rkhlqzgy
+ovtopmzu
+ahjbkzaw
+nvwfxfvb
+nqzbysna
+brexdapi
+jvwutzmq
+reoafhkr
+aeharbyc
+ymvjuqpn
+odweptpx
+mwfzfgrq
+qnbgujqe
+jdvavsiv
+ufgsatep
+utvtvndt
+xyonbztr
+ouyaeqni
+epnaskiv
+tqzwxasp
+sxudjakj
+ppbmajsr
+spqcqqad
+bulnhjfs
+debyqzym
+zledocps
+hvtwwnim
+wuvqjkmt
+vixowhlc
+synpjwrg
+yprcuicv
+wlbdrrhu
+mhzvzeit
+jfrtudaw
+ntdgtxbf
+mqvkbkha
+avzhocnk
+rvcnnzlb
+ezhvbpxp
+bgxeudlr
+snncfbun
+cceyydvf
+ergghyiv
+baywxxgm
+rluoymdj
+hjytmgxb
+eivafblf
+qotcsomv
+zstlpdeo
+imqwjhdd
+kmlkbwnj
+mquzlglq
+lrasxxzn
+afnrzblr
+ljlrzwkv
+wiiclwqv
+tbammsoi
+kitvjdiu
+twxhwnvx
+qwlrtrmz
+nsabmidx
+ysqxzaid
+qtzxvuow
+lpnzibay
+fkyfkcwh
+goaeengg
+jcgyfqjd
+avpldldh
+lsefwwxg
+cbnivbah
+znidoakv
+oiupmqhs
+gzgzxwau
+svimmpqp
+czjofzkx
+slvahvuj
+hgghpwzi
+qqytgtew
+fcdzdijf
+wfmyjjjg
+oabukfam
+bfpyjarh
+opjjzuxp
+dturiscs
+ydzjxijm
+ushhlffh
+jrhbfhvn
+ozjesxqs
+psoazeho
+czwpexgf
+aubbucmc
+fekhvfgp
+blixqivh
+dytoxjks
+geipbnaw
+fsygumdf
+fnqhvkax
+wueqmkxb
+hjvpeuie
+rirgrkxj
+lhvunqnh
+vxhcgtxy
+ljiiqwhv
+gmcdnvos
+ziwsabrz
+zdtgrffn
+mwijrxpn
+jpuxtkze
+bbufxkvy
+orbpbvyc
+cwboyilk
+nkjljrcy
+ybdwckjk
+iegfymei
+pmocypny
+tbvbuvrm
+uovwjjpt
+dzfpixwu
+pywzrlzi
+gckwatyi
+jxapzmew
+oeaiivqf
+ejtsefkd
+njxpqtwu
+hgwcbrok
+vmmgbadc
+mssfzxhn
+yhjfmfwu
+iylhiddx
+stjppdoy
+tmtdnked
+powalodl
+orydpdet
+wglrrtmz
+cmhxagub
+tzuntycz
+cnfimria
+egklaeoq
+rqsjyvck
+ixizedid
+pbmrgvge
+ggpasnuz
+mmsxlhsg
+iazvwofl
+xdbigytq
+hdubbpsh
+yfuktmiv
+qenfqhhq
+dyknyhyr
+tehlplwl
+msxxytnl
+mzzibilg
+ekndtqhx
+nrqgcebz
+kxpoopht
+uknlrvsq
+tksvulfn
+bzaerzkd
+elzunecg
+wqodfhhl
+qarirobh
+lbdihftv
+jedokhkc
+zoryeegh
+hceufcgx
+ivwexljx
+tecynrtk
+uludfkpg
+ubnvcadx
+tufdeiko
+fhlgwysq
+qkgecunv
+nkhpoefm
+vajmrmbj
+mxbhzhij
+zxknrpfa
+izijmrgl
+fhdecewz
+nerazpwu
+jpusqgni
+cycyeavi
+vdilxqje
+ldsqnfam
+dkqkfobn
+ctlrvsbr
+bkmfpzbe
+fdklbgrf
+cjjxzzlo
+zjgotpci
+jsekrolb
+xudoqzpy
+khzyxudb
+pgfxhbwp
+ghhqdkhc
+bszyorop
+qoctnrcu
+zntexpro
+yqtwijxo
+ufgoduva
+rfmvokel
+izxdabdu
+jjlcytsp
+sqbzxmei
+lbmfqhmk
+iqugozxv
+gqkerurs
+obauqiol
+vjiavpjg
+xwifisoa
+lgdsybve
+ncktlbws
+baaehiyx
+kyxrplga
+keeyjwjr
+kfwsrsmy
+obibcmpe
+dvjjhtut
+ocezenxs
+vxswiuwj
+moiacpci
+tzougogf
+jaldounz
+jlsckwfb
+dqnlxeua
+aprfnbjb
+btracexv
+yirdccur
+yilvotby
+qjokslpd
+tdxcpyrf
+mebacfie
+bxcgholo
+zsjnmohj
+rooztfuf
+rowmbvgp
+mneowyhw
+qovodyoj
+lgsoucpt
+rnfupvcq
+izgskidy
+oxjjtsfm
+hgdhcrzz
+fbhkdukc
+ofejcggd
+fygdulih
+qgycsnfp
+yshnipvg
+olwjdrxj
+ijehiskz
+anekcijz
+ygsmtmun
+nvytgoej
+xxotdtic
+eadmjgqy
+mcnzfctc
+nxcewnpp
+ezsuldpm
+aspbbhtr
+absngjpy
+bbjmxzvb
+xdchcqbt
+vskvdrmw
+fbxwizhb
+icmbajue
+rqnbgeqq
+friivchk
+zwlqorza
+tuuvdiqx
+dvjyseai
+zkixqvgt
+qirzbtte
+nwgtodef
+hjgnilug
+pmyrvary
+pdoncpni
+xdbuvkio
+exdagchd
+tmchwgzn
+xgqriqne
+hlnmloew
+uqdjxgmt
+ulaxldqc
+kkaevjap
+bxdqwdzt
+soaclkqq
+jqoqjwxw
+vrlyksim
+rapqania
+adhkvmur
+ryowhvbx
+hcxtiwqk
+jpikhedh
+hdosvskq
+vtskhlbv
+xpykokqp
+oxlenmhg
+utjipbaw
+xyqpsonc
+frqgvezn
+cijiklxw
+xwsxtrcf
+odareemq
+derdtuxx
+xopbgasq
+kuzipuhf
+eptbpoos
+jtesgetv
+vvqcxymr
+wvmtuolf
+avejonoy
+wfwizbag
+srplbnvs
+gbkrhhsd
+xupwhjjh
+glztwxno
+yyapmumi
+hzcladvm
+tqdyexyo
+egsisfti
+pjwrpcrl
+zwigknsv
+vnthscff
+wnqggfbd
+ihvysqha
+wypfzkzu
+cjjuwjef
+yoraozqu
+hrlphdth
+vhyxlkdn
+cgpzbsad
+gfxnhjnj
+kmthfrrc
+ghgrjjud
+bupqueob
+nckvualr
+qezqfqyo
+fucsogtm
+zcxhypdi
+fxmjpddm
+zsuvvyvt
+klpsmvxe
+aukywwyb
+etbmrtvo
+uxggmylj
+qlptnyyb
+awfxrlqk
+qfiuksln
+xmsuzsab
+upzskbax
+ejhlazkn
+aspugswh
+stfdbhna
+kgnhzfwa
+anbocatp
+zsqfnlkl
+mnkbtygk
+muywfyze
+vufematv
+rjnravbb
+qufwwqml
+stgxdcrs
+dotmpgqk
+mpjgmlgn
+nnbgfgbi
+paqsvfuu
+taqszxqw
+lfemniuk
+hnfqekvl
+ddarkfez
+iatzsfjg
+gjbyfwoh
+unebqbgq
+eysosujo
+smsklhaj
+gczwketm
+halhdycw
+eccopazl
+wjguoavc
+nxffamig
+qtaunyfg
+cqvlyrhw
+ktcnhgmy
+srcwxxfe
+wwjqduvj
+jgwjrbct
+persltzk
+bijmwdsv
+faqetgtt
+mwpkdibg
+kblrtgvk
+iaxjyqaz
+akvwlvkc
+wngzqcmc
+lzrmhunp
+ibzpkhia
+drqasqzz
+ycmsgmot
+tthiqsnt
+pnmqmyyb
+whbmhcaf
+payffzst
+vnmbdrfs
+okftnixy
+mlwwecbs
+pxutbxdw
+gbtjmosd
+wlffksko
+fidktsnm
+ynkbzzyz
+zrrcndqk
+fppfsayu
+sprbqnvy
+dldjkbjf
+pzkzptbg
+hkfihnfz
+vpqexioj
+pgdarlwr
+ywfhgmcy
+xtcepwtx
+pstjdxgc
+subqgesj
+rzcsmqcu
+ecamlyqr
+kmmtswus
+ivasccbd
+hftmycez
+dcqnymdw
+ruzghmqr
+nkkgiamm
+ysnwdqdi
+amaezxgi
+lgrdaimp
+uwmrkqnl
+trmrubbx
+xonawvml
+piynnxqn
+fvrikggm
+rindzfup
+edoparoq
+rfdnetab
+oavtacxp
+lwhqmzpk
+ehcwrdru
+ctgetlui
+bctqhqlo
+wrxxlmjk
+uwhwjaps
+jelhsowr
+cbwowzwy
+stqkyeqh
+aypkelrt
+comebmmj
+geqllpks
+sfdontwl
+hppmvkyl
+vuktowpp
+ocoievzs
+haxkfyjc
+dhluejeb
+wzixgwts
+symyqsna
+iuxrcerk
+pzfqujow
+urccnfyo
+lwovilmr
+lmpjjhwv
+akobwxtn
+tqbquptz
+xkuzlhwz
+waktkczt
+kvyccfrb
+wvhhelfl
+vtujfdyl
+lfhaepja
+znlbuosa
+jtmvruzq
+dwzxsnpx
+qhxmuwde
+claqvmel
+jjejbywd
+zkxftxlw
+zixnwvjk
+mhsvljsj
+fdyukgpx
+kevizule
+dkuyxtsm
+rpondvew
+xzwxowwe
+uaqpjocu
+xfysjhbz
+kmcookjd
+lkvcisxx
+xsluniob";
+        public override int Day => 6;
+        protected override void SetupTestData(int id)
+        {
+            _testData = @"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
+            _expectedResult1 = "easter";
+            _expectedResult2 = "advent";
+        }
+
+        protected override void SetupRunData()
+        {
+            _lines.Clear();
+        }
+    }
+}
