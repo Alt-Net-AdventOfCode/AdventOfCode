271798fa072a981707343920b006243c6a0e78b3
Cyrille Dupuydauby <dupdob@gmail.com>
2022-12-06 06:16:26 +0100

    Day 2022-6 done

---
+++ AdventCalendar2022/Day 6/DupdobDay6.cs
@@ -0,0 +1,83 @@
+// MIT License
+// 
+//  AdventOfCode
+// 
+//  Copyright (c) 2022 Cyrille DUPUYDAUBY
+// ---
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using AoC;
+
+namespace AdventCalendar2022;
+
+public class DupdobDay6 : SolverWithLineParser
+{
+    private string _message = string.Empty;
+
+    public override void SetupRun(Automaton automaton)
+    {
+        automaton.Day = 6;
+        automaton.RegisterTestDataAndResult("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7,1);
+        automaton.RegisterTestDataAndResult("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19,2);
+    }
+
+    public override object GetAnswer1()
+    {
+        for (var i = 3; i <_message.Length; i++)
+        {
+            if (ContainsDuplicates(_message.Substring(i-3, 4)))
+            {
+                continue;
+            }
+            return i+1;
+        }
+
+        return -1;
+    }
+
+    private static bool ContainsDuplicates(string block)
+    {
+        var hit = new HashSet<char>();
+        return block.Any(car => !hit.Add(car));
+    }
+    
+    public override object GetAnswer2()
+    {
+        for (var i = 14; i <_message.Length; i++)
+        {
+            if (ContainsDuplicates(_message.Substring(i-13, 14)))
+            {
+                continue;
+            }
+            return i+1;
+        }
+
+        return -1;
+    }
+
+    protected override void ParseLine(string? line, int index, int lineCount)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            return;
+        }
+
+        _message = line;
+    }
+}
