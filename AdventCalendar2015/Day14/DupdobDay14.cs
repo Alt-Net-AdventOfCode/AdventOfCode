417ab759ee38e82839f62d9af819b70cc717f87d
Cyrille Dupuydauby <dupdob@gmail.com>
2025-04-28 13:51:54 +0200

    Day2015-14: Done

--- AdventCalendar2015/Day14/DupdobDay14.cs
+++ AdventCalendar2015/Day14/DupdobDay14.cs
@@ -64,7 +64,7 @@ public class DupdobDay14: SolverWithIntParameter
     [Example(1, @"
 Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
 Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
-", 1120, "", 1000)]
+", 1120, 1000)]
     protected override object GetAnswer1(int totalTime = 2503)
     {
         var maxDistance = 0;
